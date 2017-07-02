8d8ba01cdb598e95169f8fee6863d20af5ad488c
ZenBrayn <rwbenz@gmail.com>
2017-07-02 04:41:02 -0700

    Updated visualization -- fully interactive now with new features

--- README.md
+++ README.md
@@ -2,11 +2,45 @@
 
 *Visualizations using the Collatz Conjecture rules*
 
-![](collatz_img.png)
+![](collatz_10-12-5-10000.png)
 
-I was inspired to try and recreate the visualization of the [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture) as shown in this [Numberphile video](https://www.youtube.com/watch?v=LqKpkdRRLZw).  I wasn't successful, but I still ended up making some interesting visualizations.
+I was inspired to recreate the visualization of the [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture) as shown in this [Numberphile video](https://www.youtube.com/watch?v=LqKpkdRRLZw).  While my results look somewhat different from those shown in the video, some interesting patterns still emerged.
 
-I used [p5.js](https://p5js.org) to create the visualization.  The drawing code is located inside `sketch.js`, and it's really quite simple.
+In this interactive visualization, the Collatz sequence is computed for each number in a continuously increasing set.  For each number, a line visualization is drawn based upon its Collatz sequence: starting in the middle of the canvas for the value of 1, turn a specified amount to the left or right depending upon the parity of the next number in the sequence and draw a line of a specific length in that direction. Repeat until all the numbers of the sequence have been processed, where the turn angle continually accumulates across the sequence.
 
-You can get a variety of interesting images by adjusting the `angle` and `maxNum` variables inside the `setup` function.
+Use the sliders to change the parameters of the visualization:
+
+* **Alpha**: level of transparency used for drawing (lower is more transparent)
+* **Angle**: the absolute value of the angle at which to turn prior to drawing the next number in the Collatz sequence
+* **Distance**: how far to draw from a Collatz sequence element to the next
+* **Speed**: how quickly to generate the Collatz sequences and associated line drawings
+
+The visualization can also be controlled by the interface buttons:
+
+* Reset: resets the visualization back to the beginning
+* Pause: pauses the generation of new Collatz sequences and line drawings
+* Play: starts/continues the generation processing
+* Jump to Number (and input field): sets the current number to process from the value in the input field
+* Cumulative Render: using the value in the input field, all numbers upto that value are processed and the resulting visualization is displayed
+* Save Img: save the visualization to a .png file
+
+The visualization was created entirely using [p5.js](https://p5js.org).
+
+---
+
+## Example Images
+
+![](collatz_10-12-5-10000.png)
+
+![](collatz_8-90-10-10000.png)
+
+![](collatz_8-73-15-10000.png)
+
+![](collatz_10-7-5-10000.png)
+
+![](collatz_25-60-8-10000.png)
+
+![](collatz_25-45-10-10000.png)
+
+
 
--- sketch.js
+++ sketch.js
@@ -1,43 +1,85 @@
 function setup() {
   // Canvas parameters
-  canvasWidth = 1600;
-  canvasHeight = 1200;
+  canvasWidth = 1400;
+  canvasHeight = 600;
   createCanvas(canvasWidth, canvasHeight);
 
   // Drawing parameters
+  strokeAlpha = 25;
+  angleMode(DEGREES);
+  angle = 12;
+  xDist = 5;
+  fr = 50;
+  frameRate(fr);
+
   smooth();
   strokeWeight(2);
   strokeCap(SQUARE);
-  strokeAlpha = 50;
+  
   stroke(255,255,255, strokeAlpha);
-	
-	// Misc drawing variables
-	angleMode(DEGREES);
-  angle = 12;
-  xDist = 10;
+  textFont("Avenir Next");
+
   cntr = 1;
+  procNums = [];
   
   // Background
   push();
-  fill(0,0,0);
   noStroke();
+  fill(0,0,0);
   rect(0, 0, canvasWidth, canvasHeight);
   pop();
   
   // Interface elements
-  alphaSlider = createSlider(0, 255, strokeAlpha);  
+  alphaSlider = createSlider(0, 255, strokeAlpha);
+  alphaSlider.position(20, 70);
+
   angleSlider = createSlider(0, 90, angle);
+  angleSlider.position(20, 100);
+
   distSlider = createSlider(5, 20, xDist);
+  distSlider.position(20, 130);
+
+  frameRateSlider = createSlider(1, 50, fr);
+  frameRateSlider.position(20, 160);
+
+  numInp = createInput();
+  numInp.position(20, 190);
+  numInp.size(75);
+
+  numButton = createButton("Jump to Number");
+  numButton.position(numInp.x * 2 + numInp.width, 191);
+  numButton.mousePressed(jumpToNumber);
+
   resetButton = createButton("Reset");
-  playButton = createButton("Play");
+  resetButton.position(20, 230);
+  resetButton.mousePressed(resetCanvas);
+
   pauseButton = createButton("Pause");
+  pauseButton.position(90, 230);
+  pauseButton.mousePressed(pauseDrawing);
+
+  playButton = createButton("Play");
+  playButton.position(170, 230);
+  playButton.mousePressed(resumeDrawing);
+
+  cumRenderButton = createButton("Cumulative Render");
+  cumRenderButton.position(20, 260);
+  cumRenderButton.mousePressed(cumRender);
+
   pngButton = createButton("Save Img");
-  
-  drawInterface();
-  
+  pngButton.position(145, 260);
+  pngButton.mousePressed(saveImg);
 }
 
-function drawInterface() {
+
+function drawInterfaceText() {
+  push();
+  stroke(255, 255, 255, 255);
+  strokeWeight(0.5);
+  fill(50, 50, 50);
+  rect(0, 0, 300, 295);
+  pop();
+
   push();
   noStroke();
   fill(255, 255, 255);
@@ -48,47 +90,30 @@ function drawInterface() {
   
   textSize(12);
   
-  // Alpha Slider
-  alphaSlider.position(20, 70);
-  text("Alpha", alphaSlider.x * 2 + alphaSlider.width, 85);
+  text("Alpha: " + alphaSlider.value(), alphaSlider.x * 2 + alphaSlider.width, 82);
+  text("Angle: " + angleSlider.value(), angleSlider.x * 2 + angleSlider.width, 112);
+  text("Distance: " + distSlider.value(), distSlider.x * 2 + distSlider.width, 142);
+  text("Speed: " + frameRateSlider.value(), frameRateSlider.x * 2 + frameRateSlider.width, 172);
   
-  // Angle Slider
-  angleSlider.position(20, 100);
-  text("Angle", angleSlider.x * 2 + angleSlider.width, 115);
-  
-  // Distance Slider
-  distSlider.position(20, 130);
-  text("Distance", distSlider.x * 2 + distSlider.width, 145);
-  
-  // Reset Button
-  resetButton.position(20, 160);
-  resetButton.mousePressed(resetCanvas);
-  
-  // Play Button
-  playButton.position(80, 160);
-  playButton.mousePressed(resumeDrawing);
-
-  // Pause Button
-  pauseButton.position(132, 160);
-  pauseButton.mousePressed(pauseDrawing);
-  
-  // Save png
-  pngButton.position(20, 190);
-  pngButton.mousePressed(saveImg);
-  
-  pop();  
+  pop();   
 }
 
-function resetCanvas() {
+function blankCanvas() {
   push();
   fill(0,0,0);
   noStroke();
   rect(0, 0, canvasWidth, canvasHeight);
-  pop();
-  
-  drawInterface();
-  
+  pop();  
+}
+
+function resetCanvas() {
+  blankCanvas();
+
   cntr = 1;
+  numInp.value(cntr);
+  procNums = [];
+
+  drawInterfaceText();
 }
 
 function pauseDrawing() {
@@ -99,36 +124,104 @@ function resumeDrawing() {
   loop();
 }
 
-function saveImg() {
-  saveCanvas("collatz_img.png");
+function jumpToNumber() {
+  num = parseInt(numInp.value());
+
+  if (isNaN(num)) {
+    num = 1;
+  }
+
+  cntr = num;
+  numInp.value(cntr);
 }
 
-function draw() {
-  // Slider params
+function updateParams() {
   strokeAlpha = alphaSlider.value();
   stroke(255, 255, 255, strokeAlpha);
   angle = angleSlider.value();
   xDist = distSlider.value();
-  	
-	seq = collatz(cntr);
-	push();
-	
-	translate(canvasWidth * 0.5, canvasHeight * 0.5);
-	for (j = 1; j < seq.length; j++) {
-	  curVal = seq[j];
-	
-	  if (curVal % 2 == 0) {
-	    rotate(-1 *angle);
-	  } else {
-	    rotate(angle);
-	  }
-	
-	  line(0, 0, xDist, 0);
-	  translate(xDist, 0);
-	}
-	
-	pop();
-	
+  fr = frameRateSlider.value();
+  frameRate(fr);
+  numInp.value(cntr);
+}
+
+function cumRender() {
+  blankCanvas();
+
+  // Reset the procNums array
+  procNums = [];
+  cntr = parseInt(numInp.value());
+  updateParams();
+
+  for (i = 1; i <= cntr; i++) {
+    procNums.push(i);
+    drawCollatzNum(i);
+  }
+
+  drawInterfaceText();
+}
+
+
+function saveImg() {
+  // Remove the interface
+  blankCanvas();
+
+  updateParams();
+
+  // Title water mark
+  push();
+  textSize(36);
+  noStroke();
+  fill(150, 150, 150);
+  text("Collatz Musings", 20, 50);
+  textSize(12);
+  fill(50, 50, 50);
+  textFont("Courier");
+  text("Alpha     : " + alphaSlider.value(), 30, 70);
+  text("Angle     : " + angleSlider.value(), 30, 85);
+  text("Distance  : " + distSlider.value(), 30, 100);
+  text("No. Values: " + procNums.length, 30, 115);
+  pop();
+
+  // Render with all the processed numbers
+  for (i = 0; i < procNums.length; i++) {
+    drawCollatzNum(procNums[i]);
+  }
+
+  imgName = "collatz_" + strokeAlpha + "-" + angle + "-" + xDist + "-" + procNums.length + ".png"
+  saveCanvas(imgName);
+
+  // Re-draw Interface
+  drawInterfaceText();
+}
+
+function drawCollatzNum(num) {
+  seq = collatz(num);
+  
+  push();
+  
+  translate(canvasWidth * 0.5, canvasHeight * 0.5);
+  for (j = 1; j < seq.length; j++) {
+    curVal = seq[j];
+  
+    if (curVal % 2 == 0) {
+      rotate(-1 *angle);
+    } else {
+      rotate(angle);
+    }
+  
+    line(0, 0, xDist, 0);
+    translate(xDist, 0);
+  }
+  
+  pop();
+}
+
+function draw() {
+  updateParams();
+  drawInterfaceText();
+  procNums.push(cntr);
+  drawCollatzNum(cntr);
   cntr += 1;
 }
 
